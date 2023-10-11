89da23234dabf702cb9a8f5af9659d435d645fdd
povrozuksn <96878944+povrozuksn@users.noreply.github.com>
2023-10-11 20:40:54 +0400

    Управление видимостью картинок
    Видимость картинок-меню по категории кнопки
    Видимость картинок в центре по клику и видимости картинок меню

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@ struct Button
 {
     int x;
     const char* name;
+    string category;
 
     void draw()
     {
@@ -36,6 +37,7 @@ struct Picture
     int w;
     int h;
     bool visible;
+    string category;
 
     void draw()
     {
@@ -45,8 +47,8 @@ struct Picture
     bool click()
     {
         return (txMouseButtons() == 1 &&
-                txMouseX() > 10 && txMouseX() < 480 &&
-                txMouseY() > y && txMouseY() < y+550);
+                txMouseX() > x && txMouseX() < x+w_scr &&
+                txMouseY() > y && txMouseY() < y+h_scr);
     }
 };
 
@@ -61,32 +63,38 @@ txDisableAutoPause();
     txCreateWindow (1200, 700);
 
     int count_btn = 3;
-    int count_pic = 6;
+    int count_pic = 9;
 
     //Область инициализации объектов
     //Кнопка
     Button btn[count_btn];
-    btn[0] = {30, "Вид одежды"};
-    btn[1] = {200, "Эмблемы"};
-    btn[2] = {370, "Логотипы"};
+    btn[0] = {30, "Вид одежды", "Одежда"};
+    btn[1] = {200, "Эмблемы", "Эмблема"};
+    btn[2] = {370, "Логотипы", "Логотип"};
 
     //Картинка-меню
     Picture menu_pic[count_pic];
-    menu_pic[0] = {10, 100, 94, 110, txLoadImage ("Pictures/Одежда/Майка.bmp"), 470, 550, false};
-    menu_pic[1] = {10, 250, 94, 110, txLoadImage ("Pictures/Одежда/Футболка.bmp"), 470, 550, false};
-    menu_pic[2] = {10, 100, 100, 100, txLoadImage ("Pictures/Эмблемы/Эмблема1.bmp"), 100, 100, false};
-    menu_pic[3] = {10, 250, 100, 100, txLoadImage ("Pictures/Эмблемы/Эмблема2.bmp"), 100, 100, false};
-    menu_pic[4] = {10, 400, 100, 100, txLoadImage ("Pictures/Эмблемы/Эмблема3.bmp"), 100, 100, false};
-    menu_pic[5] = {10, 100, 100, 100, txLoadImage ("Pictures/Логотипы/logo1.bmp"), 100, 100, false};
+    menu_pic[0] = {10, 100,  94, 110, txLoadImage ("Pictures/Одежда/Майка.bmp"), 470, 550, false, "Одежда"};
+    menu_pic[1] = {10, 250,  94, 110, txLoadImage ("Pictures/Одежда/Футболка.bmp"), 470, 550, false, "Одежда"};
+    menu_pic[2] = {10, 100, 100, 100, txLoadImage ("Pictures/Эмблемы/Эмблема1.bmp"), 100, 100, false, "Эмблема"};
+    menu_pic[3] = {10, 250, 100, 100, txLoadImage ("Pictures/Эмблемы/Эмблема2.bmp"), 100, 100, false, "Эмблема"};
+    menu_pic[4] = {10, 400, 100, 100, txLoadImage ("Pictures/Эмблемы/Эмблема3.bmp"), 100, 100, false, "Эмблема"};
+    menu_pic[5] = {10, 100, 100, 100, txLoadImage ("Pictures/Логотипы/logo1.bmp"), 100, 100, false, "Логотип"};
+    menu_pic[6] = {10, 250, 100, 100, txLoadImage ("Pictures/Логотипы/logo2.bmp"), 100, 100, false, "Логотип"};
+    menu_pic[7] = {10, 400, 100, 100, txLoadImage ("Pictures/Логотипы/logo3.bmp"), 100, 100, false, "Логотип"};
+    menu_pic[8] = {10, 550, 100, 100, txLoadImage ("Pictures/Логотипы/logo4.bmp"), 100, 100, false, "Логотип"};
 
     //Картинка в центре
     Picture centr_pic[count_pic];
-    centr_pic[0] = {430, 100, 470, 550, txLoadImage ("Pictures/Одежда/Майка.bmp"), 470, 550, false};
-    centr_pic[1] = {430, 100, 470, 550, txLoadImage ("Pictures/Одежда/Футболка.bmp"), 470, 550, false};
-    centr_pic[2] = {10, 100, 100, 100, txLoadImage ("Pictures/Эмблемы/Эмблема1.bmp"), 100, 100, false};
-    centr_pic[3] = {10, 250, 100, 100, txLoadImage ("Pictures/Эмблемы/Эмблема2.bmp"), 100, 100, false};
-    centr_pic[4] = {10, 400, 100, 100, txLoadImage ("Pictures/Эмблемы/Эмблема3.bmp"), 100, 100, false};
-    centr_pic[5] = {10, 100, 100, 100, txLoadImage ("Pictures/Логотипы/logo1.bmp"), 100, 100, false};
+    centr_pic[0] = {430, 100, 470, 550, menu_pic[0].pic, menu_pic[0].w, menu_pic[0].h, false};
+    centr_pic[1] = {430, 100, 470, 550, menu_pic[1].pic, menu_pic[1].w, menu_pic[1].h, false};
+    centr_pic[2] = {600, 100, 100, 100, menu_pic[2].pic, menu_pic[2].w, menu_pic[2].h, false};
+    centr_pic[3] = {600, 200, 100, 100, menu_pic[3].pic, menu_pic[3].w, menu_pic[3].h, false};
+    centr_pic[4] = {600, 300, 100, 100, menu_pic[4].pic, menu_pic[4].w, menu_pic[4].h, false};
+    centr_pic[5] = {600, 400, 100, 100, menu_pic[5].pic, menu_pic[5].w, menu_pic[5].h, false};
+    centr_pic[6] = {600, 500, 100, 100, menu_pic[6].pic, menu_pic[6].w, menu_pic[6].h, false};
+    centr_pic[7] = {600, 600, 100, 100, menu_pic[7].pic, menu_pic[7].w, menu_pic[7].h, false};
+    centr_pic[8] = {700, 600, 100, 100, menu_pic[8].pic, menu_pic[8].w, menu_pic[8].h, false};
 
 
     while(!txGetAsyncKeyState (VK_ESCAPE))
@@ -114,33 +122,31 @@ txDisableAutoPause();
         }
 
         //Область взаимодействия
-        if(btn[0].click())
+        for (int ib=0; ib<count_btn; ib++)
         {
-            menu_pic[0].visible = true;
-            menu_pic[1].visible = true;
+            if(btn[ib].click())
+            {
+                  for (int ip=0; ip<count_pic; ip++)
+                  {
+                        menu_pic[ip].visible = false;
+                        if (menu_pic[ip].category == btn[ib].category)
+                        {
+                            menu_pic[ip].visible = true;
+                        }
+                  }
+            }
         }
 
-        if(menu_pic[0].click())
+
+        for (int i=0; i<count_pic; i++)
         {
-            centr_pic[0].visible = true;
+            if(menu_pic[i].click() && menu_pic[i].visible)
+            {
+                centr_pic[i].visible = true;
+            }
         }
 
-        if(menu_pic[1].click())
-        {
-            centr_pic[1].visible = true;
-        }
 
-        if(btn[1].click())
-        {
-            menu_pic[2].visible = true;
-            menu_pic[3].visible = true;
-            menu_pic[4].visible = true;
-        }
-
-        if(btn[2].click())
-        {
-            menu_pic[5].visible = true;
-        }
 
         txEnd();
         txSleep(50);
@@ -149,9 +155,6 @@ txDisableAutoPause();
     for(int i=0; i<count_pic; i++)
     {
         txDeleteDC (menu_pic[i].pic);
-    }
-    for(int i=0; i<count_pic; i++)
-    {
         txDeleteDC (centr_pic[i].pic);
     }
 
